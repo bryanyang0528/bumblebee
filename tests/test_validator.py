e2bfbdec67ba626a5b6c47fe642408c1f8244951
Bryan <kenshin2004528@gmail.com>
2018-05-17 19:35:05 +0800

    add invalid data and test

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -39,3 +39,24 @@ class TestValidator(unittest.TestCase):
                          [Row(col_boolean=True, col_date=datetime.date(1995, 1, 1),
                               col_datetime=datetime.datetime(1995, 1, 1, 0, 1, 1), col_float=5566.5566,
                               col_integer=5566, col_string='string')])
+
+    def test_simple_data_validate_invalid(self):
+        data_for_simple_schema_path = data_path + 'simple_invalid.json'
+
+        with open(data_for_simple_schema_path) as f:
+            data = json.load(f)
+        simple_invalid_df = spark.createDataFrame(data)
+
+        schema = {"col_string": "string",
+                  "col_integer": "integer",
+                  "col_float": "double",
+                  "col_date": "date",
+                  "col_datetime": "timestamp",
+                  "col_boolean": "boolean"}
+
+        validate_data = Validator.validate_data(simple_invalid_df, schema).collect()
+
+        self.assertEqual(validate_data,
+                         [Row(col_boolean=True, col_date=datetime.date(1995, 1, 1),
+                              col_datetime=datetime.datetime(1995, 1, 1, 0, 1, 1), col_float=5566.5566,
+                              col_integer=None, col_string='string')])
