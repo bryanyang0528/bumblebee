20050c0c4638e44535229c7e269937705ef8cb34
Bryan <kenshin2004528@gmail.com>
2018-05-17 19:46:24 +0800

    add test for invalid date

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -55,3 +55,47 @@ class TestValidator(unittest.TestCase):
                          [Row(col_boolean=True, col_date=datetime.date(1995, 1, 1),
                               col_datetime=datetime.datetime(1995, 1, 1, 0, 1, 1), col_float=5566.5566,
                               col_integer=None, col_string='string')])
+
+    def test_simple_data_validate_invalidate_date_null(self):
+        data = [{"col_string": "string",
+                 "col_integer": "NULL",
+                 "col_float": 5566.5566,
+                 "col_date": "NULL",
+                 "col_datetime": "1995-01-01 00:01:01",
+                 "col_boolean": True}]
+        df = spark.createDataFrame(data)
+
+        schema = {"col_string": "string",
+                  "col_integer": "integer",
+                  "col_float": "double",
+                  "col_date": "date",
+                  "col_datetime": "timestamp",
+                  "col_boolean": "boolean"}
+
+        validate_data = Validator.validate_data(df, schema).collect()
+        self.assertEqual(validate_data,
+                         [Row(col_boolean=True, col_date=None,
+                              col_datetime=datetime.datetime(1995, 1, 1, 0, 1, 1), col_float=5566.5566,
+                              col_integer=None, col_string='string')])
+
+    def test_simple_data_validate_invalidate_date_format(self):
+        data = [{"col_string": "string",
+                 "col_integer": "99-01-01",
+                 "col_float": 5566.5566,
+                 "col_date": "NULL",
+                 "col_datetime": "1995-01-01 00:01:01",
+                 "col_boolean": True}]
+        df = spark.createDataFrame(data)
+
+        schema = {"col_string": "string",
+                  "col_integer": "integer",
+                  "col_float": "double",
+                  "col_date": "date",
+                  "col_datetime": "timestamp",
+                  "col_boolean": "boolean"}
+
+        validate_data = Validator.validate_data(df, schema).collect()
+        self.assertEqual(validate_data,
+                         [Row(col_boolean=True, col_date=None,
+                              col_datetime=datetime.datetime(1995, 1, 1, 0, 1, 1), col_float=5566.5566,
+                              col_integer=None, col_string='string')])
