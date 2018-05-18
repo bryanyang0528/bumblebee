1e3948240283b46d3c34497a101e5ebca3e7ae4c
Bryan <kenshin2004528@gmail.com>
2018-05-18 18:33:16 +0800

    refactor condition

--- bumblebee/reader.py
+++ bumblebee/reader.py
@@ -7,15 +7,17 @@ class HiveReader(object):
     def __init__(self):
         self.spark = SparkSession.builder.getOrCreate()
 
-    def select(self, db='default', table=None, partition=None):
+    def select(self, db='default', table=None, condition=None):
+        """
+
+        :rtype: pyspark.sql.DataFrame
+        """
         if not table:
             raise ValueError("Lack a table name to be selected")
-        elif not partition:
+        elif not condition:
             return self.spark.sql("select * from {}.{}".format(db, table))
-        elif isinstance(partition, dict) and len(partition.keys()) == 1:
-            for filed, condition in partition.items():
-                df = self.spark.sql("select * from {}.{} where {} {}".format(db, table, filed, condition))
-                return df
+        elif condition and isinstance(condition, str):
+            df = self.spark.sql("select * from {}.{} where {}".format(db, table, condition))
+            return df
         else:
             raise ValueError("Partition key is not available")
-
