c45e4ab58d727be9da6eb1f6cd64eba950947422
Bryan <kenshin2004528@gmail.com>
2018-05-18 18:15:58 +0800

    add test for reader

---
+++ tests/test_reader.py
@@ -0,0 +1,69 @@
+import unittest
+import os
+
+from mock import patch
+
+from pyspark.sql import DataFrame, SparkSession
+from bumblebee import HiveReader
+
+schema_path = 'tests/schema/'
+data_path = 'tests/data/'
+
+spark = SparkSession.builder.getOrCreate()
+
+
+class TestValidator(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    @patch('bumblebee.reader.SparkSession.sql')
+    def test_reader_is_dataframe(self, mock_spark_sql):
+        d = [{'foo':'bar'}]
+        mock_spark_sql.return_value = spark.createDataFrame(d)
+
+        reader = HiveReader()
+        db = 'default'
+        table = 'test'
+        df = reader.select(db, table)
+        mock_spark_sql.assert_called_with("select * from default.test")
+        mock_spark_sql.assert_called_once()
+        self.assertTrue(isinstance(df, DataFrame))
+
+    @patch('bumblebee.reader.SparkSession.sql')
+    def test_reader_wo_db(self, mock_spark_sql):
+        d = [{'foo':'bar'}]
+        mock_spark_sql.return_value = spark.createDataFrame(d)
+
+        reader = HiveReader()
+        table = 'test'
+        df = reader.select(table=table)
+        mock_spark_sql.assert_called_with("select * from default.test")
+        mock_spark_sql.assert_called_once()
+        self.assertTrue(isinstance(df, DataFrame))
+
+    @patch('bumblebee.reader.SparkSession.sql')
+    def test_reader_wo_table(self, mock_spark_sql):
+        d = [{'foo':'bar'}]
+        mock_spark_sql.return_value = spark.createDataFrame(d)
+
+        reader = HiveReader()
+        db = 'default'
+        with self.assertRaises(ValueError):
+            df = reader.select(db=db)
+            mock_spark_sql.assert_not_called()
+
+    @patch('bumblebee.reader.SparkSession.sql')
+    def test_reader_is_dataframe_w_partition(self, mock_spark_sql):
+        d = [{'foo': 'bar', 'dt': '2018-01-01'},
+             {'foo': 'qoo', 'dt': '2018-01-02'}]
+        mock_spark_sql.return_value = spark.createDataFrame(d)
+
+        reader = HiveReader()
+        db = 'default'
+        table = 'test'
+        partition = {"dt": "> '2018-01-01'"}
+        df = reader.select(db, table, partition=partition)
+        mock_spark_sql.assert_called_with("select * from default.test where dt > '2018-01-01'")
+        mock_spark_sql.assert_called_once()
+        self.assertTrue(isinstance(df, DataFrame))
