0f14ac14e52802994b27a1a7db17e4fd299fd93c
Bryan <kenshin2004528@gmail.com>
2018-05-18 18:33:37 +0800

    add test for invalid condition

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,5 +1,4 @@
 import unittest
-import os
 
 from mock import patch
 
@@ -62,8 +61,23 @@ class TestValidator(unittest.TestCase):
         reader = HiveReader()
         db = 'default'
         table = 'test'
-        partition = {"dt": "> '2018-01-01'"}
-        df = reader.select(db, table, partition=partition)
+        condition = "dt > '2018-01-01'"
+        df = reader.select(db, table, condition=condition)
         mock_spark_sql.assert_called_with("select * from default.test where dt > '2018-01-01'")
         mock_spark_sql.assert_called_once()
         self.assertTrue(isinstance(df, DataFrame))
+
+    @patch('bumblebee.reader.SparkSession.sql')
+    def test_read_df_w_invalid_partition_not_basestring(self, mock_spark_sql):
+        d = [{'foo': 'bar'}]
+        mock_spark_sql.return_value = spark.createDataFrame(d)
+
+        reader = HiveReader()
+        db = 'default'
+        table = 'test'
+        condition = ["dt > '2018-01-01'", "dt < '2018-01-02'"]
+        with self.assertRaises(ValueError):
+            df = reader.select(db, table, condition=condition)
+            mock_spark_sql.assert_not_called()
+
+
