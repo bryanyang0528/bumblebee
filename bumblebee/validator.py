126ac358cca2ad26781b8e5845a4b766d5b53605
Bryan <kenshin2004528@gmail.com>
2018-05-24 18:54:59 +0800

    add schema validator

--- bumblebee/validator.py
+++ bumblebee/validator.py
@@ -1,3 +1,4 @@
+import re
 
 from pyspark.sql.functions import col
 
@@ -9,3 +10,14 @@ class Validator(object):
     @classmethod
     def validate_data(cls, dataframe, schema: dict):
         return dataframe.select(*(col(c).cast(schema[c]) for c in dataframe.columns))
+
+    @classmethod
+    def validate_schema(cls, dataframe, schema_rule: str):
+
+        if schema_rule == 'bq':
+            new_column_name_list = ['_' + x if re.match(r'[^\D]', x) else x for x in dataframe.columns]
+        else:
+            raise ValueError('Did not support this rule: {}'.format(schema_rule))
+
+        dataframe = dataframe.toDF(*new_column_name_list)
+        return dataframe
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -78,3 +78,70 @@ class TestValidator(unittest.TestCase):
                          [Row(col_boolean=True, col_date=None,
                               col_datetime=datetime.datetime(1995, 1, 1, 0, 1, 1), col_float=5566.5566,
                               col_integer=None, col_string='string')])
+
+
+class TestValidatorSchema(unittest.TestCase):
+
+    simple_schema = {"1col_string": "string",
+                         "2col_integer": "integer",
+                         "3col_float": "double",
+                         "4col_date": "date",
+                         "5col_datetime": "timestamp",
+                         "6col_boolean": "boolean"}
+
+    def test_simple_data_validate_column_name(self):
+        data = [{"1col_string": "string",
+                 "2col_integer": "99-01-01",
+                 "3col_float": 5566.5566,
+                 "4col_date": "NULL",
+                 "5col_datetime": "1995-01-01 00:01:01",
+                 "6col_boolean": True}]
+        df = spark.createDataFrame(data)
+        df.printSchema()
+        validate_data = Validator.validate_data(df, self.simple_schema)
+        validate_data_schema = Validator.validate_schema(validate_data, 'bq').collect()
+        self.assertEqual(validate_data_schema,
+                         [Row(_6col_boolean=True, _4col_date=None,
+                              _5col_datetime=datetime.datetime(1995, 1, 1, 0, 1, 1), _3col_float=5566.5566,
+                              _2col_integer=None, _1col_string='string')])
+
+    def test_simple_data_validate_column_name_number_in_string(self):
+        simple_schema = {"1col_1string": "string",
+                         "2col_2integer": "integer",
+                         "3col_3float": "double",
+                         "4col_4date": "date",
+                         "5col_5datetime": "timestamp",
+                         "6col_6boolean": "boolean"}
+
+        data = [{"1col_1string": "string",
+                 "2col_2integer": "99-01-01",
+                 "3col_3float": 5566.5566,
+                 "4col_4date": "NULL",
+                 "5col_5datetime": "1995-01-01 00:01:01",
+                 "6col_6boolean": True}]
+
+        df = spark.createDataFrame(data)
+        df.printSchema()
+        validate_data = Validator.validate_data(df, simple_schema)
+        validate_data_schema = Validator.validate_schema(validate_data, 'bq').collect()
+        self.assertEqual(validate_data_schema,
+                         [Row(_6col_6boolean=True,
+                              _4col_4date=None,
+                              _5col_5datetime=datetime.datetime(1995, 1, 1, 0, 1, 1),
+                              _3col_3float=5566.5566,
+                              _2col_2integer=None,
+                              _1col_1string='string')])
+
+        def test_simple_data_validate_column_name_not_support_error(self):
+            data = [{"1col_string": "string",
+                     "2col_integer": "99-01-01",
+                     "3col_float": 5566.5566,
+                     "4col_date": "NULL",
+                     "5col_datetime": "1995-01-01 00:01:01",
+                     "6col_boolean": True}]
+            df = spark.createDataFrame(data)
+            df.printSchema()
+            validate_data = Validator.validate_data(df, self.simple_schema)
+
+            with self.assertRaise(ValueError):
+                validate_data_schema = Validator.validate_schema(validate_data, 'test').collect()
